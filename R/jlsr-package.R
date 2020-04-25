9fad35c00aa6aff634ca5231c896926d10d69a46
James Laird-Smith <jameslairdsmith@gmail.com>
2020-04-25 18:35:23 +0100

    added package documentation

---
+++ R/jlsr-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom ggplot2 ggsave
+## usethis namespace: end
+NULL
